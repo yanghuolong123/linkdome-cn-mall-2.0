ddbae27e25f9c3d389db06d6ce1b60bf4a87dc54
leiyanning <1878370355@qq.com>
2021-12-20 16:44:25 +0800

    年龄性别

--- src/views/footfall-analytics/Trajectory.vue
+++ src/views/footfall-analytics/Trajectory.vue
@@ -67,6 +67,7 @@
                 <el-image :src="img.image_path" fit="contain"></el-image>
                 <span class="time">{{ img.cur_time }}</span>
                 <span>{{ img.location }}</span>
+                <span> {{ img.object_id % 2 == 0 ? "男" : "女" }} 30-40</span>
               </div>
               <el-button
                 :disabled="
